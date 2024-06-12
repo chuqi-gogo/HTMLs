f67fd685b475bae8080dea9a920e45b14b5fafb8
Chuqi <chanshiguannb@gmail.com>
2024-06-12 12:05:08 -0700

    HUGE UPDATE

---
+++ CAREER/mycareer.js
@@ -0,0 +1,8 @@
+function changetime(){
+    document.getElementById("changetime").style.backgroundColor = "darkgray";
+    document.getElementById("changetime").style.color = "white";
+}
+function changetime1(){
+    document.getElementById("changetime").style.backgroundColor = "azure";
+    document.getElementById("changetime").style.color = "black";
+}
